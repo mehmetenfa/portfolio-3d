d09d4c0a82dd8288b3b2e4f6fba690e99d6b27aa
Mehmet Enfa Vakitsayan <106542921+mehmetenfa@users.noreply.github.com>
2023-08-22 23:01:23 +0300

    .

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,32 @@
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
-import { styles } from '../styles';
-import { navLinks } from '../constants';
-import { menu, close } from '../assets'
+import { styles } from "../styles";
+import { navLinks } from "../constants";
+import { menu, close } from "../assets";
 import logo from "../assets/logoo1.png";
 
-const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+function Navbar() {
+  const [active, setActive] = useState("");
+  const [toggle, setToggle] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      if (scrollTop > 100) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return <div>Navbar</div>;
 }
 
-export default Navbar
+export default Navbar;
