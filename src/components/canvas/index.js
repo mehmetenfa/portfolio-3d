40f111d429a0f92c6cf52a1af2f9f9c897425804
Mehmet Enfa Vakitsayan <106542921+mehmetenfa@users.noreply.github.com>
2023-08-25 21:51:40 +0300

    canvas Folder

---
+++ src/components/canvas/index.js
@@ -0,0 +1,6 @@
+import EarthCanvas from './Earth'
+import BallCanvas from './Ball'
+import ComputersCanvas from './Computers'
+import StartsCanvas from './Starts'
+
+export { EarthCanvas, BallCanvas, ComputersCanvas, StartsCanvas };
